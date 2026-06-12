ad5d08da21ce6c384316dcbc0ae6495450394bf0
FireWat <semiaosimon@gmail.com>
2026-06-12 14:28:19 +0200

    Creating admin dashboard panel

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use App\Entity\Game;
+use App\Entity\Genre;
+use App\Entity\Review;
+use App\Entity\WishlistItem;
+
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
@@ -25,6 +31,11 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkTo(SomeCrudController::class, 'The Label', 'fas fa-list');
+        yield MenuItem::linkTo(EditorCrudController::class,'Users', 'fa-solid fa-users');
+        yield MenuItem::linkTo(EditorCrudController::class,'Games', 'fa-solid fa-gamepad');
+        yield MenuItem::linkTo(EditorCrudController::class,'Genres', 'fa-solid fa-tags');
+        yield MenuItem::linkTo(EditorCrudController::class,'Review', 'fa-solid fa-star');
+        yield MenuItem::linkTo(EditorCrudController::class,'Wishlist', 'fa-solid fa-heart' );
+
     }
 }
